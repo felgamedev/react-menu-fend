cade09bca3157e54dbe1c8f0dfa6b3f78d49c895
Scott Hampton <felgamedev@gmail.com>
2018-10-16 09:10:38 -0300

    Ran Beautify to, highlights flow for null allergens

--- menu-fend/src/components/ui/AllergenList.js
+++ menu-fend/src/components/ui/AllergenList.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class AllergenListSimple extends Component {
   state = {
     userAllergens: []
   }
 
-  render(){
+  render() {
     let {allAllergens, config, onDeleteAllergen} = this.props
-    return (
-      <div className="allergen-list-simple" style={{display: "flex", background: "#ddd", flexWrap: "wrap", flexDirection: "row", justifyContent: "flex-start"}}>{allAllergens ?
-      allAllergens.map(allergen => (
-        <div key={allergen._id} style={{background: "white", borderRadius: 20, margin: 4, padding: 8}}>
-          {allergen.name}{config.canDelete && (<div style={
-            {display: "inline", background: "#ccc", marginLeft: 4, borderRadius: "50%", padding: 4, textAlign: "center"}}
-            className="allergen-remove-x"
-            onClick={(e) => onDeleteAllergen(e, allergen)}>x</div>)}</div>))
-        : <span>No Allergens found</span>}</div>
-    )
+    return (<div className="allergen-list-simple" style={{
+        display: "flex",
+        background: "#ddd",
+        flexWrap: "wrap",
+        flexDirection: "row",
+        justifyContent: "flex-start"
+      }}>
+      {
+        allAllergens
+          ? allAllergens.map(allergen => (<div key={allergen._id} style={{
+              background: "white",
+              borderRadius: 20,
+              margin: 4,
+              padding: 8
+            }}>
+            {allergen.name}{
+              config.canDelete && (<div style={{
+                  display: "inline",
+                  background: "#ccc",
+                  marginLeft: 4,
+                  borderRadius: "50%",
+                  padding: 4,
+                  textAlign: "center"
+                }} className="allergen-remove-x" onClick={(e) => onDeleteAllergen(e, allergen)}>x</div>)
+            }</div>))
+          : <span>No Allergens found</span>
+      }</div>)
   }
 
 }

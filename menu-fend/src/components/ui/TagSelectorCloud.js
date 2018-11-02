16b9be7a1634d7bb157100afb0273374acbe3f02
felgamedev <felgamedev@gmail.com>
2018-11-02 12:49:48 -0300

    Labelled Tag Cloud Component for tracking binary selections
    Selection status is added and bubbled up to parent component for use in 
    forms

---
+++ menu-fend/src/components/ui/TagSelectorCloud.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import './TagSelectorCloud.css'
+
+class TagSelectorCloud extends Component {
+  state = {
+    tags: null
+  }
+
+  componentWillMount(){
+    var tags = this.props.tags.map(tag => {
+      tag.selected = false
+      return tag
+    })
+
+    this.setState({
+      tags
+    })
+  }
+
+  componentDidUpdate(){
+  }
+
+  onTagClicked(tag){
+    const tags = this.state.tags
+
+    for(let i = 0; i < tags.length; i++){
+      if(tags[i] === tag) tags[i].selected = !tags[i].selected
+    }
+
+    this.setState({
+      tags
+    })
+  }
+
+  render(){
+    return (
+      <div className="tag-selector-cloud">
+        {this.props.title && (<h3 className="cloud-title">{this.props.title}</h3>)}
+        {this.props.tags && this.props.tags.map(tag => (<span className={tag.selected ? "cloud-tag selected" : "cloud-tag"} key="tags.name" onClick={(e) => this.onTagClicked(tag)}>{tag.name}</span>))}
+      </div>
+    )
+  }
+}
+
+export default TagSelectorCloud

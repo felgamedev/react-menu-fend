59a80d4558fdfb31982611ed40682678777e0423
felgamedev <felgamedev@gmail.com>
2018-11-03 13:17:25 -0300

    Moving to separate forms for ingredient and foodcomponent

---
+++ menu-fend/src/components/ui/FoodComponentForm.js
@@ -0,0 +1,245 @@
+import React, { Component } from 'react'
+import { TagSelectorCloud, TagSelectorSingleLine } from './TagSelectorCloud'
+import './IngredientForm.css'
+
+var baseUrl = "http://localhost:8000/api/v1/"
+
+class IngredientForm extends Component {
+  state = {
+    nameValue: '',
+    brandNameValue: '',
+    allIngredients: null,
+    usersIngredients: null,
+    nameMatchFound: false,
+    brandNameMatchFound: false,
+    allAllergens: [],
+    allergenMap: null,
+    // Single/Component Mode variables
+    singleIngredientMode: false,
+    componentIngredients: [],
+    componentNameValue: '',
+    componentBrandNameValue: ''
+  }
+
+  componentWillMount(){
+    // TODO if user is logged in, retrieve the list of ingredients they have added
+    if(this.props.user !== null){
+      console.log("User logged in, retrieving their ingredients");
+      // Use the /u prefix to specify new route to handle find by user id
+      fetch(baseUrl + 'ingredient/u/' + this.props.user._id, {
+        mode: "cors", headers: { "Content-type": "application/json"}
+      })
+      .then(response => response.json())
+      .then(data => this.setState({usersIngredients: data}))
+    }
+
+    var self = this
+    var ingredients = null
+    var allergens = null
+    var allergenMap = new Map()
+
+    // Retrieve all ingredients and allergens in a chain to create a map and save to state
+    fetch(baseUrl + 'ingredient', {
+      mode: "cors", headers: { "Content-type": "application/json"}
+    })
+    .then(response => response.json())
+    .then(data => ingredients = data)
+    .then(() =>
+      {
+        // Retrieve all allergens
+        return fetch(baseUrl + 'allergen', {
+          mode: "cors", headers: { "Content-Type": "application/json"}
+        })
+      }
+    )
+    .then(response => response.json())
+    .then(data => {
+      // Add a selected flag for each allergen for use in the form
+      allergens = data
+    })
+    .then(() => {
+      for(let i = 0; i < allergens.length; i++){
+        allergenMap.set(allergens[i]._id, allergens[i])
+      }
+
+      self.setState({
+        allIngredients: ingredients,
+        allAllergens: allergens,
+        allergenMap
+      })
+    })
+  }
+
+  onNameChanged(e){
+    let string = e.target.value
+
+    this.checkForMatches(string, this.state.brandNameValue, null)
+
+    // Store the value in state
+    this.setState({
+      nameValue: string
+    })
+  }
+
+  onBrandNameChanged(e){
+    let string = e.target.value
+
+    this.setState({
+      brandNameValue: string
+    })
+    this.checkForMatches(this.state.nameValue, string, null)
+  }
+
+  onAllergenSelectionChanged(e){
+    let selectedAllergen = e.target.value
+    let allergens = this.state.allAllergens
+
+    for(let i = 0; i < allergens.length; i++){
+      allergens[i].selected = (allergens[i]._id === selectedAllergen) ? !allergens[i].selected : allergens[i].selected
+    }
+
+    this.setState({
+      allAllergens: allergens
+    })
+  }
+
+  checkForMatches(name, bname){
+    let { allIngredients, nameMatchFound, brandNameMatchFound } = this.state
+    let nm = false, bnm = false
+    // Check for match
+    for(let i = 0; i < allIngredients.length; i++){
+      if(name !== null && name.trim().toLowerCase() === allIngredients[i].name.trim().toLowerCase()){
+        nm = true
+        console.log("Checking name");
+      }
+
+      if(bname !== null && bname.trim().toLowerCase() === allIngredients[i].brandName.toLowerCase()) bnm = true
+    }
+
+    // Set the state for button state
+    if(nm !== nameMatchFound) this.setState({ nameMatchFound: nm })
+    if(bnm !== brandNameMatchFound) this.setState({ brandNameMatchFound: bnm })
+
+  }
+
+  onSubmitForm(e){
+    e.preventDefault()
+    let { nameValue: name, brandNameValue: brandName, allIngredients, allAllergens } = this.state
+
+    let allergensArray = []
+    allAllergens.forEach(allergen => {
+      if(allergen.selected) allergensArray.push(allergen._id)
+    })
+
+    let data = JSON.stringify({ name: name, brandName: brandName, allergens: allergensArray })
+
+    fetch(baseUrl + "ingredient", {
+      method: "POST",
+      mode: "cors",
+      body: data,
+      headers: { "Content-Type" : "application/json"}
+    })
+    .then(response => response.json())
+    .then(data => {
+      allIngredients.push(data)
+      for(let i = 0; i < allAllergens.length; i++){
+        allAllergens[i].selected = false
+      }
+      this.setState({
+        allIngredients,
+        nameValue: '',
+        brandNameValue: '',
+        formSubmitDisabled: false
+      })
+    })
+  }
+
+  toggleInputMode(singleIngredientMode){
+      this.setState({
+        singleIngredientMode
+      })
+  }
+
+  render(){
+    let { allIngredients, nameValue, brandNameValue,
+      nameMatchFound, brandNameMatchFound, allAllergens, allergenMap } = this.state
+
+    let selectedAllergens = []
+    allAllergens.forEach(allergen => {
+      if(allergen.selected) selectedAllergens.push("" + allergen._id)
+    })
+
+    if(allIngredients) allIngredients.sort((a, b) => (a.name.toUpperCase() === b.name.toUpperCase() ? 0 : (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1))
+
+    //if(allIngredients) console.log(allergenMap.get(allIngredients[4].allergens[0].acronym));
+    return(
+      <div className={(allIngredients === null) ? "ingredient-form-conatiner-disabled" : "ingredient-form-container"}>
+        <h2>Add New Ingredient</h2>
+
+        <div className="input-mode-selector-container">
+          <div className="input-mode-selector" style={{backgroundColor: (this.state.singleIngredientMode ? "LightGreen" : "White") }} onClick={(e) => this.toggleInputMode(true)}>Single Ingredient</div>
+          <div className="input-mode-selector" style={{backgroundColor: (this.state.singleIngredientMode ? "white" : "LightGreen") }} onClick={(e) => this.toggleInputMode(false)}>Premade Ingredient</div>
+        </div>
+
+        {this.state.singleIngredientMode &&
+          (<form className="ingredient-form-single" onSubmit={e => this.onSubmitForm(e)}>
+            <label>
+              <p>Name:
+              <input type="text" value={nameValue} onChange={(e) => this.onNameChanged(e)} disabled={false} placeholder="e.g. Oat flour"/></p>
+            </label>
+            <label>
+              <p>Brand Name:
+              <input type="text" value={brandNameValue} onChange={(e) => this.onBrandNameChanged(e)} disabled={!String(nameValue).length > 0} placeholder="e.g. Big Mill" /></p>
+            </label>
+            {/*-- Allergens Cloud Selector-- */}
+            {allAllergens.length > 0 && <TagSelectorCloud tags={allAllergens} title="Allergens"/>}
+            <br/>
+
+            <button disabled={(nameValue === '') || (nameMatchFound && brandNameMatchFound)} type="submit">Submit</button>
+          </form>)
+        }
+
+        {!this.state.singleIngredientMode && (
+          <form className="ingredient-form-multiple" onSubmit={e => this.onSubmitForm(e)}>
+            <label>
+              <p>Name:
+              <input type="text" value={nameValue} onChange={(e) => this.onNameChanged(e)} disabled={false} placeholder="e.g. Yellow Mustard"/></p>
+            </label>
+            <label>
+              <p>Brand Name:
+              <input type="text" value={brandNameValue} onChange={(e) => this.onBrandNameChanged(e)} disabled={!String(nameValue).length > 0} placeholder="e.g. Heinz" /></p>
+            </label>
+            <label>
+            <p>Ingredients:
+            <input type="text" /></p>
+
+            <label>
+              <TagSelectorSingleLine tags={this.state.componentIngredients} title="Component Ingredients"/>
+            </label>
+
+            </label>
+            {/*-- Allergens Cloud Selector-- */}
+            {allAllergens.length > 0 && <TagSelectorCloud tags={allAllergens} title="Allergens"/>}
+            <br/>
+
+            <button disabled={(nameValue === '') || (nameMatchFound && brandNameMatchFound)} type="submit">Submit</button>
+          </form>
+        )}
+
+
+        <h3>Temporary Ingredient List</h3>
+        {allIngredients && allIngredients.map(ingredient =>
+          <div key={ingredient._id}>
+          {ingredient.name}{ingredient.brandName && " - "}{ingredient.brandName && (<span>{ingredient.brandName}</span>)}
+          {ingredient.allergens.length > 0 && ingredient.allergens.map(allergen => (<span key={allergen}> ({allergenMap.get(allergen).acronym})</span>))}
+        </div>)}
+        <h3>Ingredient Inspector</h3>
+
+
+
+
+      </div>)
+  }
+}
+
+export default IngredientForm
--- menu-fend/src/components/ui/IngredientForm.js
+++ menu-fend/src/components/ui/IngredientForm.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
-import TagSelectorCloud from './TagSelectorCloud'
+import SingleIngredientForm from './SingleIngredientForm'
+import { TagSelectorCloud, TagSelectorSingleLine } from './TagSelectorCloud'
+import './IngredientForm.css'
 
 var baseUrl = "http://localhost:8000/api/v1/"
 
 class IngredientForm extends Component {
   state = {
-    nameValue: '',
-    brandNameValue: '',
     allIngredients: null,
     usersIngredients: null,
-    nameMatchFound: false,
-    brandNameMatchFound: false,
-    allAllergens: [],
-    allergenMap: null
+    allAllergens: null,
+    allergenMap: null,
+    // Single/Component Mode variables
+    singleIngredientMode: true,
+    componentIngredients: [],
+    componentNameValue: '',
+    componentBrandNameValue: ''
   }
 
   componentWillMount(){
@@ -64,73 +67,18 @@ class IngredientForm extends Component {
     })
   }
 
-  onNameChanged(e){
-    let string = e.target.value
-
-    this.checkForMatches(string, this.state.brandNameValue, null)
-
-    // Store the value in state
-    this.setState({
-      nameValue: string
-    })
-  }
-
-  onBrandNameChanged(e){
-    let string = e.target.value
-
-    this.setState({
-      brandNameValue: string
-    })
-    this.checkForMatches(this.state.nameValue, string, null)
-  }
-
-  onAllergenSelectionChanged(e){
-    let selectedAllergen = e.target.value
-    let allergens = this.state.allAllergens
-
-    for(let i = 0; i < allergens.length; i++){
-      allergens[i].selected = (allergens[i]._id === selectedAllergen) ? !allergens[i].selected : allergens[i].selected
-    }
-
-    this.setState({
-      allAllergens: allergens
-    })
-  }
-
-  checkForMatches(name, bname){
-    let { allIngredients, nameMatchFound, brandNameMatchFound } = this.state
-    let nm = false, bnm = false
-    // Check for match
-    for(let i = 0; i < allIngredients.length; i++){
-      if(name !== null && name.trim().toLowerCase() === allIngredients[i].name.trim().toLowerCase()){
-        nm = true
-        console.log("Checking name");
-      }
-
-      if(bname !== null && bname.trim().toLowerCase() === allIngredients[i].brandName.toLowerCase()) bnm = true
-    }
-
-    // Set the state for button state
-    if(nm !== nameMatchFound) this.setState({ nameMatchFound: nm })
-    if(bnm !== brandNameMatchFound) this.setState({ brandNameMatchFound: bnm })
+  onIngredientSubmitted(ingredient){
 
   }
 
-  onSubmitForm(e){
-    e.preventDefault()
-    let { nameValue: name, brandNameValue: brandName, allIngredients, allAllergens } = this.state
-
-    let allergensArray = []
-    allAllergens.forEach(allergen => {
-      if(allergen.selected) allergensArray.push(allergen._id)
-    })
-
-    let data = JSON.stringify({ name: name, brandName: brandName, allergens: allergensArray })
+  // Must be bound to this
+  onSubmitSingleIngredientForm(data){
+    const { allIngredients, allAllergens} = this.state
 
     fetch(baseUrl + "ingredient", {
       method: "POST",
       mode: "cors",
-      body: data,
+      body: JSON.stringify(data),
       headers: { "Content-Type" : "application/json"}
     })
     .then(response => response.json())
@@ -148,39 +96,30 @@ class IngredientForm extends Component {
     })
   }
 
-  render(){
-    let { allIngredients, nameValue, brandNameValue,
-      nameMatchFound, brandNameMatchFound, allAllergens, allergenMap } = this.state
+  toggleInputMode(singleIngredientMode){
+      this.setState({
+        singleIngredientMode
+      })
+  }
 
-    let selectedAllergens = []
-    allAllergens.forEach(allergen => {
-      if(allergen.selected) selectedAllergens.push("" + allergen._id)
-    })
+  render(){
+    let { allIngredients, allAllergens, allergenMap } = this.state
 
     if(allIngredients) allIngredients.sort((a, b) => (a.name.toUpperCase() === b.name.toUpperCase() ? 0 : (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1))
 
     //if(allIngredients) console.log(allergenMap.get(allIngredients[4].allergens[0].acronym));
     return(
-      <div className={(allIngredients === null) ? "ingredient-form-disabled" : "ingredient-form"}>
-        <h2>Ingredients</h2>
+      <div className={(allIngredients === null) ? "ingredient-form-conatiner-disabled" : "ingredient-form-container"}>
+        <h2>Add New Ingredient</h2>
 
-        <form onSubmit={e => this.onSubmitForm(e)}>
-          <h3>Add new Ingredient</h3>
-          <label>
-            <p>Name:
-            <input type="text" value={nameValue} onChange={(e) => this.onNameChanged(e)} disabled={false} hint="Enter name here"/></p>
-          </label>
-          <label>
-            <p>Brand Name:
-            <input type="text" value={brandNameValue} onChange={(e) => this.onBrandNameChanged(e)} disabled={!String(nameValue).length > 0} hint="Enter brand ame if applicable" /></p>
-          </label>
-          {/*-- Allergens Cloud Selector-- */}
-          {allAllergens.length > 0 && <TagSelectorCloud tags={allAllergens} title="Allergens"/>}
-          <br/>
+        <div className="input-mode-selector-container">
+          <div className="input-mode-selector" style={{backgroundColor: (this.state.singleIngredientMode ? "LightGreen" : "White") }} onClick={(e) => this.toggleInputMode(true)}>Single Ingredient</div>
+          <div className="input-mode-selector" style={{backgroundColor: (this.state.singleIngredientMode ? "white" : "LightGreen") }} onClick={(e) => this.toggleInputMode(false)}>Premade Ingredient</div>
+        </div>
 
-          <button disabled={(nameValue === '') || (nameMatchFound && brandNameMatchFound)} type="submit">Submit</button>
-        </form>
-
+        {(this.state.singleIngredientMode && allAllergens) && /* Check for allAllergens as its presence means ingredients should already be loaded in chained promise */
+          (<SingleIngredientForm allAllergens={allAllergens} allIngredients={allIngredients} onSubmit={this.onSubmitSingleIngredientForm.bind(this)}/>)
+        }
 
         <h3>Temporary Ingredient List</h3>
         {allIngredients && allIngredients.map(ingredient =>
@@ -188,11 +127,6 @@ class IngredientForm extends Component {
           {ingredient.name}{ingredient.brandName && " - "}{ingredient.brandName && (<span>{ingredient.brandName}</span>)}
           {ingredient.allergens.length > 0 && ingredient.allergens.map(allergen => (<span key={allergen}> ({allergenMap.get(allergen).acronym})</span>))}
         </div>)}
-        <h3>Ingredient Inspector</h3>
-
-
-
-
       </div>)
   }
 }

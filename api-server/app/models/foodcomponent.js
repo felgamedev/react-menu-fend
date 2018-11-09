dc9e3f34ac44e68169fa48d2d0247ac12c09d992
felgamedev <felgamedev@gmail.com>
2018-11-09 13:59:46 -0400

    Added Food Component route to API

--- api-server/app/models/foodcomponent.js
+++ api-server/app/models/foodcomponent.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const IngredientSchema = require('./ingredient')
 
 const FoodComponentSchema = {
   name: String,
-  ingredients: [{}]
+  brandName: String,
+  ingredients: []
 }
 
 module.exports = mongoose.model('FoodComponent', FoodComponentSchema)
